ca0d4729e095b6748cf9c868a2fa8d53f6f2e1bf
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-04-09 00:49:41 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -66,10 +66,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", None
+    "SUPPORT_CHANNEL", "https://t.me/nakama_asl"
 )  # Example:- https://t.me/nakama_asl
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", None
+    "SUPPORT_GROUP", "https://t.me/virtual_executive"
 )  # Example:- https://t.me/virtual_executive
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
